72fc67d18be2f2cb59c9b1d0c64d52e589d5d368
Emerson <77053593+Emerson916@users.noreply.github.com>
2022-05-07 22:20:45 -0300

    releasing cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: "https://nlwreturn-production-61af.up.railway.app/"
 }));
 app.use(express.json());
 app.use(router);
