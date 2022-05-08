2eacb494e497732b5f62a7ddb749322f8c560b6b
Emerson <77053593+Emerson916@users.noreply.github.com>
2022-05-07 21:30:43 -0300

    env port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,6 @@ app.use(cors({
 app.use(express.json());
 app.use(router);
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Servidor rodando na porta: 3001");
 });
